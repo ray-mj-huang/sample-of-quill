014d87acb66e89be90341a7988976748f20bf8a4
ray-mj-huang <ray.mj.huang@gmail.com>
2025-04-21 14:43:11 +1000

    feat: add title field for post

--- src/feaures/post/Editor.tsx
+++ src/feaures/post/Editor.tsx
@@ -1,20 +1,26 @@
 import { useDispatch, useSelector } from 'react-redux';
 import ReactQuill from 'react-quill-new'
 import 'react-quill-new/dist/quill.snow.css'
-import { setPostContent, PostData } from './postSlice'
+import { setPostTitle, setPostContent, PostData } from './postSlice'
 import { RootState } from '../../store';
 
 function Editor() {
   const dispatch = useDispatch()
+  const title = useSelector((state: RootState) => state.post.title)
   const content = useSelector((state: RootState) => state.post.content)
 
-  const handleChange = (value: PostData['content']) => {
+  const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(setPostTitle(e.target.value)) 
+  }
+  
+  const handleContentChange = (value: PostData['content']) => {
     dispatch(setPostContent(value)) 
   }
 
   return (
-    <div style={{ background: '#242424', width: '100%', minHeight: '100vh',display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <ReactQuill theme="snow" value={content} onChange={handleChange}/>
+    <div style={{ background: '#242424', width: '100%', minHeight: '100vh',display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+      <input type="text" value={title} onChange={handleTitleChange} />
+      <ReactQuill theme="snow" value={content} onChange={handleContentChange}/>
     </div>
   )
 }
--- src/feaures/post/postSlice.ts
+++ src/feaures/post/postSlice.ts
@@ -2,10 +2,12 @@ import { createSlice } from '@reduxjs/toolkit'
 import type { PayloadAction } from '@reduxjs/toolkit'
 
 export interface PostData {
+  title: undefined | string
   content: undefined | string
 }
 
 const initialState: PostData = {
+  title: undefined,
   content: undefined,
 }
 
@@ -13,12 +15,15 @@ export const postSlice = createSlice({
   name: 'post',
   initialState,
   reducers: {
+    setPostTitle: (state, action: PayloadAction<PostData['title']>) => {
+      state.title = action.payload
+    },
     setPostContent: (state, action: PayloadAction<PostData['content']>) => {
       state.content = action.payload
     },
   },
 })
 
-export const { setPostContent } = postSlice.actions
+export const { setPostTitle, setPostContent } = postSlice.actions
 
 export default postSlice.reducer
